4bb4ed758f5635ec408aa989bb038e2421f92fd4
thesologlobesightseer-jscoder <thesologlobesightseer@gmail.com>
2023-12-31 14:59:47 +0530

    Env updates

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function Document() {
   return (
@@ -13,8 +11,6 @@ export default function Document() {
       <body className="bg-white text-gray-700 antialiased">
         <Main />
         <NextScript />
-        <Analytics />
-        <SpeedInsights />
       </body>
     </Html>
   )
